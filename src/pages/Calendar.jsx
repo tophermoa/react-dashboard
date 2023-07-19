ba47720a9cf26225dd293b258327e8271ab5ea4f
tophermoa <moasukasuka@gmail.com>
2023-07-19 23:26:56 +0700

    add calendar n kanban

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { ScheduleComponent, Day, Week, WorkWeek, Month, Agenda, Inject, Resize, DragAndDrop} from '@syncfusion/ej2-react-schedule'
+
+import {scheduleData} from '../data/dummy'
+import { Header } from '../components'
 
 const Calender = () => {
   return (
-    <div>Calender</div>
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header title="Calendar" category="App" />
+      <ScheduleComponent
+        height="650px"
+        eventSettings={{
+          dataSource: scheduleData
+        }}
+        selectedDate={new Date(2021, 0, 10)}
+      >
+        <Inject services={[Day, Week, WorkWeek, Month, Agenda, Resize, DragAndDrop]} />
+      </ScheduleComponent>
+    </div>
   )
 }
 
